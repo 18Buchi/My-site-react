55fab501ee263c86c81e0a2ae0210c93366054c7
18Buchi <taka25803690@gmail.com>
2021-03-20 09:51:59 +0900

    delete

---
+++ src/components/SidebarOutLink.js
@@ -0,0 +1,32 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarOutLink = [
+  {
+    title: "Instagram",
+    src: "https://www.instagram.com/18buchi/",
+    icon: <AiIcons.AiFillInstagram />,
+    cName: "nav-text",
+  },
+  {
+    title: "twitter",
+    src: "https://twitter.com/18Buchi/",
+    icon: <FaIcons.FaTwitterSquare />,
+    cName: "nav-text",
+  },
+  {
+    title: "github",
+    src: "https://github.com/18Buchi",
+    icon: <FaIcons.FaGithubSquare />,
+    cName: "nav-text",
+  },
+  {
+    title: "suzuri",
+    src: "https://suzuri.jp/Buchi18",
+    icon: <AiIcons.AiTwotoneCheckSquare />,
+    cName: "nav-text",
+  },
+
+];
